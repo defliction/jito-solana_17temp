5c57e11b3390b49539c847205542664ee3c5a8e5
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2023-12-02 17:16:55 +0100

    [JIT-1710] - Optimize Bundle Consumer Checks (#490)

--- tip-distributor/src/stake_meta_generator_workflow.rs
+++ tip-distributor/src/stake_meta_generator_workflow.rs
@@ -400,7 +400,7 @@ mod tests {
             vec![INITIAL_VALIDATOR_STAKES; 3],
         );
 
-        let bank = Bank::new_for_tests(&genesis_config);
+        let (mut bank, _) = Bank::new_with_bank_forks_for_tests(&genesis_config);
 
         /* 2. Seed the Bank with [TipDistributionAccount]'s */
         let merkle_root_upload_authority = Pubkey::new_unique();
@@ -589,7 +589,6 @@ mod tests {
             ))
         }
 
-        let mut bank = Arc::new(bank);
         let mut stake_pubkeys = validator_0_delegations
             .iter()
             .map(|v| v.stake_account_pubkey)
